5d2278fe538b2a822bd4092c9f273c3977163014
Max Lopez <familiacachorin@gmail.com>
2023-07-11 11:26:13 -0700

    add digits to calculator and create functions

--- calculator.js
+++ calculator.js
@@ -1,11 +1,31 @@
 let num1;
 let num2
 let operation;
+let displayValue;
+const display = document.querySelector('.display');
+const calcButtons = document.querySelectorAll('.calcButton');
+calcButtons.forEach((item) => {
+    item.addEventListener('click', button)
+});
+
+//flow 
+
+function buttonPressed(e) {
+    switch(e.target.id) {
+        case clear:
+            num1 = "o";
+            num2 = "o";
+
+    }
+}
+function updateDisplay() {
+    display.innerHTML = num1 + " " + operation + " " + num2;
+}
 
 function operate(num1, num2) {
     return null;
 }
-
+//operations
 function add (a, b) {
     return a + b;
 }
